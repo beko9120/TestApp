b060696b2b8ca84bf82eabe419c8c7ae31b4c070
berik2031 <berik2031@gmail.com>
2022-06-02 19:44:36 +0600

    fix todoService

---
+++ TodoApiDTO.DAL/Context/TodoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApiDTO.DAL.Context
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext()
+        {
+        }
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        protected TodoContext(DbContextOptions options)
+          : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
